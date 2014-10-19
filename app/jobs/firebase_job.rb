717966d6872a34001543169dc808c084d70bc55a
Chris Bolton <c@chrisbolton.me>
2014-10-18 23:06:56 -0400

    intercom update

--- app/jobs/firebase_job.rb
+++ app/jobs/firebase_job.rb
@@ -2,7 +2,6 @@ class FirebaseJob
   include SuckerPunch::Job
 
   def perform(key, values)
-    firebase = Firebase::Client.new(ENV['FIREBASE_URL'])
     HTTParty.post("#{ENV['FIREBASE_URL']}/#{key}.json?auth=#{ENV['FIREBASE_SECRET']}", { body: values.to_json })
   end
 end
