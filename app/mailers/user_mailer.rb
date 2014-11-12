432aae7dfbceb3d7b123d04581c66fe71a3b8bb9
Chris Bolton <c@chrisbolton.me>
2014-11-12 12:53:11 -0800

    minor fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -19,7 +19,7 @@ class UserMailer < ActionMailer::Base
 
   def mailchimp email
     gb = Gibbon::API.new(ENV['MAILCHIMP_API'])
-    gb.lists.subscribe({:id => 'ee35460d90', :email => {:email => email.email}, :double_optin => false})
+    gb.lists.subscribe({:id => 'ee35460d90', :email => {:email => email}, :double_optin => false})
   end
 
 end
