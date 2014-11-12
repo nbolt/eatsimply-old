75d2d7740007745828c92e19e6d0cd2e2cfde6bf
Chris Bolton <c@chrisbolton.me>
2014-11-12 12:42:39 -0800

    minor fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,12 @@
 class UserMailer < ActionMailer::Base
 
   def new_email email
-    mailchimp(email) if Rails.env == 'production'
+    mailchimp(email.email) if Rails.env == 'production'
   	mail(subject: "Thanks from Eatt!", to: email.email, from: 'support@ea.tt', tag: 'welcome-preview', track_opens: true)
   end
 
   def new_vegas_email email
-    mailchimp(email) if Rails.env == 'production'
+    mailchimp(email.email) if Rails.env == 'production'
   	mail(subject: "Thanks from Eatt!", to: email.email, from: 'support@ea.tt', tag: 'welcome-preview', track_opens: true)
   end
 
@@ -19,7 +19,7 @@ class UserMailer < ActionMailer::Base
 
   def mailchimp email
     gb = Gibbon::API.new(ENV['MAILCHIMP_API'])
-    gb.lists.subscribe({:id => 'ee35460d90', :email => {:email => email}, :double_optin => false})
+    gb.lists.subscribe({:id => 'ee35460d90', :email => {:email => email.email}, :double_optin => false})
   end
 
 end
